8db319249f8557e63ad2025fa8a89b094cbcdf1d
isaac-keepquiet <yang.isaac.jiao+github@gmail.com>
2021-11-16 10:30:32 +1100

    Add Table

---
+++ Table/Table.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<div class="container mt-3">
+  <h2>Basic Table</h2>
+  <p>The .table class adds basic styling (light padding and horizontal dividers) to a table:</p>            
+  <table class="table table-bordered table-striped">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Price</th>
+        <th>QTY</th>
+        <th>Total price</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td><p>Apple</p></td>
+        <td><input type="number" name="points" min="0" max="10"></td>
+        <td><input type="number" name="points" min="0" max="10"></td>
+        <td><input type="number" name="points" min="0" max="10"></td>
+      </tr>
+      <tr>
+        <td><p>Watermelon</p></td>
+        <td><input type="number" name="points" min="0" max="10"></td>
+        <td><input type="number" name="points" min="0" max="10"></td>
+        <td><input type="number" name="points" min="0" max="10"></td>
+      </tr>
+      <tr>
+        <td><p>Orenge</p></td>
+        <td><input type="number" name="points" min="0" max="10"></td>
+        <td><input type="number" name="points" min="0" max="10"></td>
+        <td><input type="number" name="points" min="0" max="10"></td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+</body>
+</html>
