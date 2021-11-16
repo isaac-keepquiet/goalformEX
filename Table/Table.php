5f0fa7ca8125a81e752af4375025bb3cf45c1a21
isaac-keepquiet <yang.isaac.jiao+github@gmail.com>
2021-11-16 11:01:40 +1100

    Modify total price

--- Table/Table.php
+++ Table/Table.php
@@ -6,12 +6,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
 <div class="container mt-3">
-  <h2>Basic Table</h2>
-  <p>The .table class adds basic styling (light padding and horizontal dividers) to a table:</p>            
+  <h2>Fruit Price Table</h2>
+  <p></p>            
   <table class="table table-bordered table-striped">
     <thead>
       <tr>
@@ -26,22 +27,31 @@
         <td><p>Apple</p></td>
         <td><input type="number" name="points" min="0" max="10"></td>
         <td><input type="number" name="points" min="0" max="10"></td>
-        <td><input type="number" name="points" min="0" max="10"></td>
+        <td></td>
       </tr>
       <tr>
         <td><p>Watermelon</p></td>
         <td><input type="number" name="points" min="0" max="10"></td>
         <td><input type="number" name="points" min="0" max="10"></td>
-        <td><input type="number" name="points" min="0" max="10"></td>
+        <td></td>
       </tr>
       <tr>
         <td><p>Orenge</p></td>
         <td><input type="number" name="points" min="0" max="10"></td>
         <td><input type="number" name="points" min="0" max="10"></td>
-        <td><input type="number" name="points" min="0" max="10"></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td><h3>Total</h3></td>
+        <td></td>
+        <td></td>
+        <td></td>
       </tr>
     </tbody>
   </table>
+  <div class="submit-button">
+    <button class="btn btn-primary"><P>SUBMIT</P></button>
+  </div>
 </div>
 
 </body>
