97a0032c913bbc4e84228e8119517f22120a42b7
isaac-keepquiet <yang.isaac.jiao+github@gmail.com>
2021-11-18 10:29:49 +1100

    all total price

--- Table/Table.php
+++ Table/Table.php
@@ -3,7 +3,8 @@
 <head>
   <title>Bootstrap Example</title>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -29,24 +30,24 @@
           <td><p>Apple</p></td>
           <td><input type="number" class="num-input pr price" id="p1" name="points" onchange="add_to_total(this)" min="0" max="100"></td>
           <td><input type="number" class="num-input pr qty" id="qyt1" name="points" onchange="add_to_total(this)" min="0" max="1000"></td>
-          <td><input type="number" name=""  class="num-input pr total_price" onchange="add_all_total(this)"</td>
+          <td><input type="number" name=""  class="num-input pr total_price" id="totalprice1" onchange="add_all_total(this)"</td>
         </tr>
         <tr>
           <td><p>Watermelon</p></td>
           <td><input type="number" class="num-input pr price" id="p2" name="points" onchange="add_to_total(this)" min="0" max="100"></td>
           <td><input type="number" class="num-input pr qty" id="qyt2" name="points" onchange="add_to_total(this)" min="0" max="1000"></td>
-          <td><input type="number" name=""  class="num-input pr total_price" onchange="add_all_total(this)"></td>
+          <td><input type="number" name=""  class="num-input pr total_price" id="totalprice2" onchange="add_all_total(this)"></td>
         </tr>
         <tr>
           <td><p>Orenge</p></td>
           <td><input type="number" class="num-input pr price" id="p3" name="points" onchange="add_to_total(this)" min="0" max="100"></td>
           <td><input type="number" class="num-input pr qty" id="qyt3" name="points" onchange="add_to_total(this)" min="0" max="1000"></td>
-          <td><input type="number" name=""  class="num-input pr total_price" onchange="add_all_total(this)"></td>
+          <td><input type="number" name=""  class="num-input pr total_price" id="totalprice3" onchange="add_all_total(this)"></td>
         </tr>
         <tr>
           <td><h3>Total</h3></td>
           <td></td>
-          <td><td><input type="number" name=""  class="num-input pr all_total_price"></td></td>
+          <td><td><input type="number" name=""  class="num-input pr all_total_price" id="alltotalprice"></td></td>
         </tr>
       </tbody>
     </table>
--- Table/script.js
+++ Table/script.js
@@ -4,8 +4,33 @@ function add_to_total(nu){
     var qty = parent.find('.qty').val() == "" ? 1 : parent.find('.qty').val();
     var total = price * qty;
     parent.find('.total_price').val(total);
-  }
 
-//   function add_all_total(num){
+    // var total_1 = document.getElementById("totalprice1");
+    // var total_2 = document.getElementById("totalprice2");
+    // var total_3 = document.getElementById("totalprice3");
 
-//   }
+    // if(total_1.value!=''){
+    //     alltotalprice.value=parseInt(total_1.value);
+    // }
+}
+
+function add_all_total(sum){
+    var total_1 = $("#totalprice1").val() == "";
+     // var total_1 = document.getElementById("totalprice1");
+    var total_2 = document.getElementById("totalprice2");
+    var total_3 = document.getElementById("totalprice3");
+
+    if(total_1.value!=''){
+        alltotalprice.value=parseInt(total_1.value);
+    }
+    
+}
+
+
+// function add_all_total(num){
+//     var total_1
+//     var total_2
+//     var total_3
+//     var alltotal = total_1 + total_2 + total_3;
+//     parent.find('.all_total_price').val(alltotal);
+// }
